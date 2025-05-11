f813c62e574e7179efd5749bd98f877c73425ecc
Amit Kumar <amitsingh16201814@gmail.com>
2025-05-12 00:25:21 +0530

    Fix SQLite compatibility issue for Streamlit deployment

--- knowledge_base.py
+++ knowledge_base.py
@@ -1,3 +1,10 @@
+import sys
+try:
+    import pysqlite3
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+
 import chromadb
 from chromadb.config import Settings
 import pandas as pd
